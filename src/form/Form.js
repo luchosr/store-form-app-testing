f150773acb61759ba4c7e29c9e70875b54a20596
Luciano Ramello <luchosr@gmail.com>
2022-06-10 15:37:18 +0200

    Update form component

--- src/form/Form.js
+++ src/form/Form.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { TextField } from '@mui/material';
 
 const Form = () => {
   return (
     <div>
       <h1>Create product</h1>
+
+      <form>
+        <TextField label='name' id='name' />
+      </form>
     </div>
   );
 };
--- src/form/Form.test.js
+++ src/form/Form.test.js
@@ -2,14 +2,19 @@ import React from 'react';
 
 import { screen, render } from '@testing-library/react';
 
-import Form from './form';
+import Form from './Form';
 
 describe('when the form is mounted', () => {
   it('there must be a create product form page', () => {
     render(<Form />);
-
     expect(
       screen.getByRole('heading', { name: /create product/i })
     ).toBeInTheDocument();
+  });
+
+  it('should exists the  fields: name, size, type (electronic, furniture, clothing) and a submit button.', () => {
+    render(<Form />);
+
+    expect(screen.getByLabelText(/name/i)).toBeInTheDocument();
   });
 });
